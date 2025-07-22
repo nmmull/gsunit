eb06f6947d1b8a9ddb3e6ada3f04e476771194db
Nathan Mull <nmmull@gmail.com>
2025-07-22 19:02:26 -0400

    don't check for flag

--- lib/gsunit.ml
+++ lib/gsunit.ml
@@ -7,6 +7,7 @@ module Suite = Suite
 module Gradescope = Gradescope
 
 let run
+      ?(debug=false)
       ?(ounit_test_runner=default_ounit_test_runner)
       ?output
       ?visibility
@@ -21,9 +22,7 @@ let run
       ?extra_data
       suite
   in
-  if Array.exists
-       ((=) "-gsunit-to-ounit")
-       Sys.argv
+  if debug
   then OUnit2.run_test_tt_main (Suite.to_ounit_test suite)
   else
     let ounit_results =
